eca23a4192e15f5f251a596bc7b0def2095fe805
Godlumen <cicasa@163.com>
2018-01-02 16:43:02 +0800

    office finished

--- src/main/java/com/godlumen/shiro/MyShiroRealm.java
+++ src/main/java/com/godlumen/shiro/MyShiroRealm.java
@@ -40,6 +40,7 @@ public class MyShiroRealm extends AuthorizingRealm {
 //        String password=String.valueOf(token.getPassword());
         //获取用户的输入的账号.
         String mobile = (String)token.getPrincipal();
+        System.out.println(token.toString());
         System.out.println(token.getCredentials());
         //通过mobile从数据库中查找 UserLogin对象，如果找到，没找到.
         //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
